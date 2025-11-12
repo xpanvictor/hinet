7d38ee6a7e3a495ed0349e404f549187804c850a
xpanvictor <xpanvictor@gmail.com>
2025-11-12 16:12:56 +0100

    feat(app): sets up compile & runtime cli

--- apps/cli/src/main.rs
+++ apps/cli/src/main.rs
@@ -1,15 +1,21 @@
+use clap::Parser;
+use cli::commands::{Commands, HinetCli};
 use root::runtime;
 
 #[tokio::main]
 async fn main() {
     println!("System initialize..");
     let rt = runtime::Runtime::run();
-    match rt.await {
-        Ok(_) => {
-            println!("stable run")
-        }
-        Err(_) => {
-            eprintln!("some error occurred")
-        }
+    let hinet_cli = HinetCli::parse();
+
+    match &hinet_cli.commands {
+        Commands::Start(sargs) => match rt.await {
+            Ok(_) => {
+                println!("stable run")
+            }
+            Err(e) => {
+                eprintln!("error running hinet: {}", e);
+            }
+        },
     }
 }
